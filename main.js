a44295f244ebc6fe2fab769997b9f63806058abb
jeffersonjuni <jeftsantos76@gmail.com>
2024-12-24 10:34:34 -0300

    melhorias no script

--- main.js
+++ main.js
@@ -2,8 +2,13 @@ function changeIcon(element, newSrc) {
     element.querySelector('img').src = newSrc;
 }
 
-window.addEventListener("pageshow", (event) => {
-    if (event.persisted) {
-        window.location.reload(); // Recarrega a página completamente
+// Remove o estado de foco dos links ao recarregar ou retornar à página
+window.addEventListener("pageshow", () => {
+    const activeElement = document.activeElement;
+
+    // Verifica se o elemento ativo é um link ou botão e remove o foco
+    if (activeElement.tagName === "A" || activeElement.tagName === "BUTTON") {
+        activeElement.blur();
     }
 });
+
