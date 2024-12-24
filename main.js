77ca1539a8956e961e93b687ba7c04e0d38d6856
jeffersonjuni <jeftsantos76@gmail.com>
2024-12-24 10:26:42 -0300

    atualização caminhos mobile e script

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 function changeIcon(element, newSrc) {
     element.querySelector('img').src = newSrc;
 }
+
+window.addEventListener("pageshow", (event) => {
+    if (event.persisted) {
+        window.location.reload(); // Recarrega a página completamente
+    }
+});
